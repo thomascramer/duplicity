431258dc702fd0816066074cd92aa3c44df9ed8e
Chris Aumann <me@chr4.org>
2013-02-01 16:58:33 +0100

    migrate to new zabbix recipe

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ license          'GNU Public License 3.0'
 description      'Installs/Configures duplicity'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-depends          'zabbix_agent'
+depends          'zabbix'
 depends          'sudoers'
