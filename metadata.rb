a95a6de04f4f0bf75cd372e2ef0a369645826c09
Chris Aumann <me@chr4.org>
2013-05-02 13:02:48 +0200

    bump version to 0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'me@chr4.org'
 license          'GNU Public License 3.0'
 description      'Installs/Configures duplicity'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 depends          'zabbix'
 depends          'sudo'
